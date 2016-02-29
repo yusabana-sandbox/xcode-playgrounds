c39a975f99911fbfdb87d57e0033375efae59235
Yuji Takaesu <taka.esu.user@gmail.com>
2016-02-29 09:16:45 +0900

    update Regexp playground

--- Regexp.playground/Contents.swift
+++ Regexp.playground/Contents.swift
@@ -2,8 +2,41 @@
 
 import UIKit
 
-
 var str = "Hello, playground"
-
-
 print(str)
+
+
+import Foundation
+
+class Regexp {
+    let internalRegexp: NSRegularExpression
+    let pattern: String
+    
+    init(_ pattern: String) {
+        self.pattern = pattern
+        self.internalRegexp = try! NSRegularExpression( pattern: pattern, options: NSRegularExpressionOptions.CaseInsensitive)
+    }
+    
+    func isMatch(input: String) -> Bool {
+        let matches = self.internalRegexp.matchesInString( input, options: [], range:NSMakeRange(0, input.characters.count) )
+        return matches.count > 0
+    }
+    
+    func matches(input: String) -> [String]? {
+        if self.isMatch(input) {
+            let matches = self.internalRegexp.matchesInString( input, options: [], range:NSMakeRange(0, input.characters.count) )
+            var results: [String] = []
+            for i in 0 ..< matches.count {
+                results.append( (input as NSString).substringWithRange(matches[i].range) )
+            }
+            return results
+        }
+        return nil
+    }
+}
+
+var pattern = "bhoge$"
+
+var moji = "def hello(name)"
+
+Regexp(pattern).isMatch("hogefugahoge")
